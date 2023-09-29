a1c0ef1825b920197cc1ab2385e4e56d70c5a116
Dhanaraj <88299676+Dhanarajb@users.noreply.github.com>
2023-09-29 22:59:21 +0530

    Update state.js

--- state.js
+++ state.js
@@ -74,14 +74,168 @@ State is managed and maintained within a component.
 It can be modified using the setState method, and changes to the state trigger component re-renders.
 State is used for storing data that can change over time due to user interactions, events, or other factors.
 State is mutable (it can change).
-In summary, props are used for passing data from parent to child components, while state is used for managing data within a component itself. 
-  Stateful components can update their state based on user interactions or other events, leading to dynamic and interactive user interfaces.
 
-Remember that you should use state for data that is local to a component and doesn't need to be shared with other components, while props are 
-  used to pass data between components in a unidirectional flow of data.
+-------------------------------------------------------------------------
+1. Create a React class component that initializes a state variable count to 0 and displays it in a div element. Provide a button that increments count when clicked.
 
 
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+}
+2. Rewrite the previous example using a functional component with the useState hook.
 
 
+import React, { useState } from 'react';
 
-Regenerate
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+3. Create a functional component that takes an initial count as a prop and displays it. Add a button to increment the count.
+
+
+import React, { useState } from 'react';
+
+function Counter({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+4. Implement a checkbox component that toggles between "Checked" and "Unchecked" based on its state.
+
+
+import React, { useState } from 'react';
+
+function Checkbox() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p>{isChecked ? 'Checked' : 'Unchecked'}</p>
+    </div>
+  );
+}
+5. Create a component that allows you to add and remove items from a list using state.
+
+
+import React, { useState } from 'react';
+
+function ListManager() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+
+  const addItem = () => {
+    setItems([...items, newItem]);
+    setNewItem('');
+  };
+
+  const removeItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => removeItem(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+6. Create a stopwatch component that displays the elapsed time in seconds and has start, stop, and reset buttons.
+
+
+import React, { useState, useEffect } from 'react';
+
+function Stopwatch() {
+  const [running, setRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [running]);
+
+  const startStop = () => {
+    setRunning(!running);
+  };
+
+  const reset = () => {
+    setTime(0);
+    setRunning(false);
+  };
+
+  return (
+    <div>
+      <p>Elapsed Time: {time} seconds</p>
+      <button onClick={startStop}>{running ? 'Stop' : 'Start'}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
