f709396d623ada077f493660c892238ffbcd8544
Dhanaraj <88299676+Dhanarajb@users.noreply.github.com>
2023-10-19 00:02:33 +0530

    Create tpcs.js

---
+++ tpcs.js
@@ -0,0 +1,162 @@
+You can learn ReactJS easily 😄
+
+Here's all you need to get started 🙌
+
+1.Components
+• Functional Components
+• Class Components
+• JSX (JavaScript XML) Syntax
+
+2.Props (Properties)
+• Passing Props
+• Default Props
+• Prop Types
+
+3.State
+• useState Hook
+• Class Component State
+• Immutable State
+
+4.Lifecycle Methods (Class Components)
+• componentDidMount
+• componentDidUpdate
+• componentWillUnmount
+
+5.Hooks (Functional Components)
+• useState
+• useEffect
+• useContext
+• useReducer
+• useCallback
+• useMemo
+• useRef
+• useImperativeHandle
+• useLayoutEffect
+
+6.Event Handling
+• Handling Events in Functional Components
+• Handling Events in Class Components
+
+7.Conditional Rendering
+• if Statements
+• Ternary Operators
+• Logical && Operator
+
+8.Lists and Keys
+• Rendering Lists
+• Keys in React Lists
+
+9.Component Composition
+• Reusing Components
+• Children Props
+• Composition vs Inheritance
+
+10.Higher-Order Components (HOC)
+• Creating HOCs
+• Using HOCs for Reusability
+
+11.Render Props
+• Using Render Props Pattern
+
+12.React Router
+• <BrowserRouter>
+• <Route>
+• <Link>
+• <Switch>
+• Route Parameters
+
+13.Navigation
+• useHistory Hook
+• useLocation Hook
+
+State Management
+14.Context API
+• Creating Context
+• useContext Hook
+
+15.Redux
+• Actions
+• Reducers
+• Store
+• connect Function (React-Redux)
+
+16.Forms
+• Handling Form Data
+• Controlled Components
+• Uncontrolled Components
+
+17.Side Effects
+• useEffect for Data Fetching
+• useEffect Cleanup
+
+18.AJAX Requests
+• Fetch API
+• Axios Library
+
+Error Handling
+19.Error Boundaries
+• componentDidCatch (Class Components)
+• ErrorBoundary Component (Functional
+Components)
+
+20.Testing
+• Jest Testing Framework
+• React Testing Library
+
+21. Best Practices
+• Code Splitting
+• PureComponent and React.memo
+• Avoiding Reconciliation
+• Keys for Dynamic Lists
+
+22.Optimization
+• Memoization
+• Profiling and Performance Monitoring
+
+23. Build and Deployment
+• Create React App (CRA)
+• Production Builds
+• Deployment Strategies
+
+Frameworks and Libraries
+24.Styling Libraries
+• Styled-components
+• CSS Modules
+
+25.State Management Libraries
+• Redux
+• MobX
+
+26.Routing Libraries
+• React Router
+• Reach Router
+
+---------------- END -----------------
+
+Best resources to learn React quickly
+
+1.Documentation
+
+Official Updated React Documentation
+react.dev
+MDN Web Docs
+shorturl.at/tCL69
+
+2. YouTube Channel's
+
+• Traversy Media: https://lnkd.in/ga5zshJa
+• The Net Ninja: youtube.com/c/TheNetNinja
+• FreeCodeCamp: youtube.com/c/FreeCodeCamp
+• Academind: youtube.com/c/Academind
+
+3. Books
+
+• React: Up & Running - by Stoyan Stefanov
+t.ly/IpeQU
+• React.js Essentials: A fast-paced journey
+t.ly/mqPmc
+
+4. Course with certification
+t.ly/76Vhg
+
+Feel free to add anything I missed 🤝
